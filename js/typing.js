dce15cdd0334cd0b12690546025b29836b8d8361
Hari <harikrishnanru04@gmail.com>
2021-05-14 13:02:01 +0530

    few changes

--- js/typing.js
+++ js/typing.js
@@ -53,6 +53,6 @@ window.onload = function () {
   // INJECT CSS
   var css = document.createElement("style");
   css.type = "text/css";
-  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #572e91 }";
+  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #00aeef }";
   document.body.appendChild(css);
 };
